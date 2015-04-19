720216ac75affc5d0a227de1dc77a859662822fc
vikas-parashar <svnitvikas@gmail.com>
2015-04-20 00:35:05 +0530

    added ubuntu notification

--- flipkart.py
+++ flipkart.py
@@ -3,21 +3,26 @@ import requests
 import subprocess
 import os,sys
 import time
+from urlparse import urlparse
 
-def pricecheck():
-	r = requests.get("http://www.flipkart.com/cooler-master-notepal-l1-cooling-pad/p/itmdyh7fzpggufdc")
-	# r = requests.get("http://vikasparashar.in/")
+def pricecheck(url, min_price):
+	parsed = urlparse(url)
+	parsed_url = "http://"+parsed.netloc+parsed.path
+	print parsed_url
+	r = requests.get(parsed_url)
 	data = r.text
 	soup = BeautifulSoup(data)
 	for price in soup.find_all(itemprop="price"):
 		p = price.get('content')
 		p = p.replace(',','')
-		if int(p) < 1000:
-			subprocess.Popen(['vlc', '-vvv', '/home/vicodin/Music/Music/eminem/01 Rap God.mp3'])
+		if int(p) < min_price:
+			subprocess.Popen(['notify-send', "It's time to buy. price has droped"])
 			exit()
 		else:
 			print "checking...", p
 			time.sleep(3600)
 			pricecheck()
-
-pricecheck()
+url = raw_input("paste url here: ")
+min_price = int(raw_input("type minimum price: "))
+message = "It's time to buy. price has droped below", min_price
+pricecheck(url, min_price)
